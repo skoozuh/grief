62e41e4feaf9acb57c980627392866c7536fa5d8
skoozuh <itsmenamics44@gmail.com>
2025-08-07 20:03:25 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Optional: subtle cursor gem trail
-document.addEventListener('pointermove', e => {
-  const dot = document.createElement('div');
-  dot.className = 'trail';
-  dot.style.left = `${e.pageX}px`;
-  dot.style.top = `${e.pageY}px`;
-  document.body.appendChild(dot);
-  setTimeout(() => dot.remove(), 500);
-});
